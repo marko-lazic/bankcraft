b0ed1d900bb6b5d94d1a3f516b8a4c172ba7ca94
Lazić <zila015@gmail.com>
2019-07-25 21:27:19 +0200

    Initial commit

---
+++ desktop/src/net/bankcraft/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package net.bankcraft.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import net.bankcraft.BankCraftGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(BankCraftGame(), config)
+    }
+}
